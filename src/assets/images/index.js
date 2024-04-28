d17c48d12e42e27aadf2f0a94d68be1f5a8c0941
Ghada <stare.ghada@gmail.com>
2024-04-28 13:22:32 +0200

    Building the app

---
+++ src/assets/images/index.js
@@ -0,0 +1,46 @@
+import bigWatche1 from './big-watche1.png'
+import bigWatche2 from './big-watche2.png'
+import bigWatche3 from './big-watche3.png'
+
+import customer1 from './customer1.jpeg'
+import customer2 from './customer2.svg'
+
+import footerLogo from './footer-logo.svg'
+import headerLogo from './header-logo.svg'
+
+import offer from './offer.svg'
+
+import watche4 from './watche4.svg'
+import watche5 from './watche5.svg'
+import watche6 from './watche6.svg'
+import watche7 from './watche7.svg'
+import watche8 from './watche8.svg'
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailwatche1 from './thumbnail-watche1.svg'
+import thumbnailwatche2 from './thumbnail-watche2.svg'
+import thumbnailwatche3 from './thumbnail-watche3.svg'
+
+export {
+    bigWatche1,
+    bigWatche2,
+    bigWatche3,
+
+    customer1,
+    customer2,
+
+    footerLogo,
+    headerLogo,
+
+    offer,
+    watche4,
+    watche5,
+    watche6,
+    watche7,
+    watche8,
+
+    thumbnailBackground,
+    thumbnailwatche1,
+    thumbnailwatche2,
+    thumbnailwatche3
+}
