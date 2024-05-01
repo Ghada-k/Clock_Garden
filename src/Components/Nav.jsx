5a46f5a92f98ce30646ec2081efe552eb30ba6ee
Ghada <stare.ghada@gmail.com>
2024-05-01 19:39:53 +0200

    NavBar

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,27 @@
+import {headerLogo} from  '../assets/images';
+import {hamburger} from '../assets/icons';
+import { navLinks } from '../constans';
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className='flex justify-between items-center max-container'>
+        <a  className="navbar-brand" href="/">
+          <img src={headerLogo} alt="Logo" width={230} height={29} />
+        </a>
+        <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+          {navLinks.map((item)=>(
+            <li key={item.label}>
+              <a href={item.href} className="font-mono errat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className='hidden max-lg:block'>
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
   )
 }
 
--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,30 +1,30 @@
-import bigWatche1 from './big-watche1.png'
-import bigWatche2 from './big-watche2.png'
-import bigWatche3 from './big-watche3.png'
+import bigwatche1 from './big-watche1.png'
+import bigwatche2 from './big-watche2.png'
+import bigwatche3 from './big-watche3.png'
 
-import customer1 from './customer1.jpeg'
-import customer2 from './customer2.svg'
+import customer1 from './customer1.jpg'
+import customer2 from './customer2.jpg'
 
-import footerLogo from './footer-logo.svg'
-import headerLogo from './header-logo.svg'
+import footerLogo from './footerlogo.png'
+import headerLogo from './headerlogo.png'
 
-import offer from './offer.svg'
+import offer from './offer.png'
 
-import watche4 from './watche4.svg'
-import watche5 from './watche5.svg'
-import watche6 from './watche6.svg'
-import watche7 from './watche7.svg'
-import watche8 from './watche8.svg'
+import watche4 from './watche4.png'
+import watche5 from './watche5.png'
+import watche6 from './watche6.png'
+import watche7 from './watche7.png'
+import watche8 from './watche8.png'
 
 import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailwatche1 from './thumbnail-watche1.svg'
-import thumbnailwatche2 from './thumbnail-watche2.svg'
-import thumbnailwatche3 from './thumbnail-watche3.svg'
+import thumbnailwatche1 from './big-watche1.png'
+import thumbnailwatche2 from './big-watche2.png'
+import thumbnailwatche3 from './big-watche3.png'
 
 export {
-    bigWatche1,
-    bigWatche2,
-    bigWatche3,
+    bigwatche1,
+    bigwatche2,
+    bigwatche3,
 
     customer1,
     customer2,
